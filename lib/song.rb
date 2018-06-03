7c575063c51e8932df8680bb52bd1f817bec2e13
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-06-03 19:03:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,9 @@ class Sobg
   attr_accessor :name, :artist, :genre
   @@count = 0
 
-  def initialize(name, artist, genre) 
+  def initialize(name, artist, genre)
     @@count += 1
-    @name = name 
-    @genre = genre 
-end 
+    @name = name
+    @genre = genre
+  end  
+end
